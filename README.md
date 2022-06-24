b5cebf4e9c9b48f6ed08bc7197ee2e4356886576
Isaac Watts <isaacw@modere.com>
2022-06-24 17:19:21 -0600

    added project link to readme and female state lists

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rabbit_problem
+
+https://www.reddit.com/r/dailyprogrammer/comments/7s888w/20180122_challenge_348_easy_the_rabbit_problem/
--- rabbits.ts
+++ rabbits.ts
@@ -18,22 +18,47 @@ class rabbit {
 class population {
 	maleRabbitTotal: number;
 	femaleRabbitTotal: number;
-	rabbitTotal: number;
+	months: number = 0;
 
-	Male_rabbits: number;
-	Female_rabbits: number;
-	Rabbits_needed_alive: number;
+	maleRabbits: number;
+	childFemaleRabbits: number;
+	fertileFemaleRabbits: number;
+	rabbitsNeededAlive: number;
 
 	constructor(males: number, females: number, popSize: number) {
-		this.Male_rabbits = males;
-		this.Female_rabbits = females;
-		this.Rabbits_needed_alive = popSize;
+		this.maleRabbits = males;
+		this.fertileFemaleRabbits = females;
+		this.rabbitsNeededAlive = popSize;
+		this.rabbitTotal();
+	}
+
+	rabbitTotal(): number {
+		return (
+			this.maleRabbits +
+			this.fertileFemaleRabbits +
+			this.childFemaleRabbits
+		);
+	}
+
+	populationGrowth() {
+		this.rabbitTotal();
+		let desiredPopSize = this.rabbitsNeededAlive;
+
+		if (
+			this.rabbitTotal() < desiredPopSize &&
+			this.fertileFemaleRabbits > 0 &&
+			this.maleRabbits > 0
+		) {
+			this.childFemaleRabbits = 9 * this.fertileFemaleRabbits;
+			this.maleRabbits += 5 * this.maleRabbits;
+		}
 	}
 }
 
 /* 
     rabbits reproduce
         - 14 offspring (5 males and 9 females)
+        - 2 months old for females to be fertile.
     age (exclude newborns)
         - 1 month increments
     die (96 months old)
